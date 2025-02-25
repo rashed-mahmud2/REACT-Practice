ab4d7effa01bc53a2a1159d14fd110d18e6a1fbe
rashed-mahmud2 <mahamud2071@gmail.com>
2025-02-25 12:26:00 +0600

    I have learn about react state, recall and rerendering

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,79 +1,42 @@
+import { useState } from "react";
 // import logo from "./logo.svg";
 import "./App.css";
-import BioData from "./components/BioData";
+// import BioData from "./components/BioData";
 
-const bioData = [
-  {
-    name: "SR Setu",
-    email: "srsetu@gmail.com",
-    phone: "+8801234567890",
-    github: "github/srsetu",
-    skills: ["Js", "WP", "REACT", "NEXT", "Node"],
-    interests: ["Football", "Cricket", "Chese"],
-    socialLinks: [
-      { platform: "FB", handle: "fb/srsetu" },
-      { platform: "Twitter", handle: "x/srsetu" },
-      { platform: "linkdin", handle: "linkdin/in/srsetu" },
-    ],
-  },
-  {
-    name: "Hasin Hayder",
-    email: "srsetu@gmail.com",
-    phone: "+8801234567890",
-    github: "github/srsetu",
-    skills: ["Js", "WP", "REACT", "NEXT", "Node"],
-    interests: ["Football", "Cricket", "Chese"],
-    socialLinks: [
-      { platform: "FB", handle: "fb/srsetu" },
-      { platform: "Twitter", handle: "x/srsetu" },
-      { platform: "linkdin", handle: "linkdin/in/srsetu" },
-    ],
-  },
-  {
-    name: "Asife Mahir",
-    email: "srsetu@gmail.com",
-    phone: "+8801234567890",
-    github: "github/srsetu",
-    skills: ["Js", "WP", "REACT", "NEXT", "Node"],
-    interests: ["Football", "Cricket", "Chese"],
-    socialLinks: [
-      { platform: "FB", handle: "fb/srsetu" },
-      { platform: "Twitter", handle: "x/srsetu" },
-      { platform: "linkdin", handle: "linkdin/in/srsetu" },
-    ],
-  },
-  {
-    name: "Rashed Mahmud",
-    email: "srsetu@gmail.com",
-    phone: "+8801234567890",
-    github: "github/srsetu",
-    skills: ["Js", "WP", "REACT", "NEXT", "Node"],
-    interests: ["Football", "Cricket", "Chese"],
-    socialLinks: [
-      { platform: "FB", handle: "fb/srsetu" },
-      { platform: "Twitter", handle: "x/srsetu" },
-      { platform: "linkdin", handle: "linkdin/in/srsetu" },
-    ],
-  },
-];
-
+// re-rendering of react;
 function App() {
+  console.log("i am called");
+  const [dynamicCounter, setDynamicCounter] = useState(20);
+  const [dynamicCounter2, setDynamicCounter2] = useState(30);
+
+  const increaseHandler = () => {
+    setDynamicCounter(dynamicCounter + 1);
+  };
+
+  const decreaseHandler = () => {
+    setDynamicCounter(dynamicCounter - 1);
+  };
+
+  const increaseHandler2 = () => {
+    setDynamicCounter2(dynamicCounter2 + 1);
+  };
+
+  const decreaseHandler2 = () => {
+    setDynamicCounter2(dynamicCounter2 - 1);
+  };
+
   return (
     <div className="App">
-      {bioData.map((singleBioData) => (
-        <>
-          <BioData
-            name={singleBioData.name}
-            email={singleBioData.email}
-            phone={singleBioData.phone}
-            github={singleBioData.github}
-            skills={singleBioData.skills}
-            interests={singleBioData.interests}
-            socialLinks={singleBioData.socialLinks}
-          />
-          <hr />
-        </>
-      ))}
+      <div className="counter-app">
+        <p>The value of the counter is {dynamicCounter}</p>
+        <button onClick={increaseHandler}>Increase By 1</button>
+        <button onClick={decreaseHandler}>Decrease By 1</button>
+      </div>
+      <div className="counter-app2">
+        <p>The value of the counter is {dynamicCounter2}</p>
+        <button onClick={increaseHandler2}>Increase By 1</button>
+        <button onClick={decreaseHandler2}>Decrease By 1</button>
+      </div>
     </div>
   );
 }
